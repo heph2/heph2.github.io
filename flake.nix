f9593bc3c805434d185a2da99aeb566b6620bbf9
Heph <srht@mrkeebs.eu>
2022-08-02 19:39:40 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Pinky Studios site portfolio deployment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ bashInteractive hugo rsync ];
+        buildInputs = [ ];
+      };
+    });
+}
